c301616d789593e7784ef3bc248c74b5a9fa801f
Ayoub Es-sehayli <ayoub1essehayli@outlook.com>
2021-07-27 09:22:20 +0000

    Adding the DbContext Service to the Startup Class

---
+++ InternManagement.Api/ConnectionConfig.cs
@@ -0,0 +1,9 @@
+namespace InternManagement.Api
+{
+  public class ConnectionConfig
+  {
+    public string Server { get; set; }
+    public string Database { get; set; }
+    public string Password { get; set; }
+  }
+}
