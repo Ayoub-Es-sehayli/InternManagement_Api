ccfe3d9e18ff9bae1695d7fa7fac404067c9b417
wiamES <wiam.essaadi.20@gmail.com>
2021-08-11 14:21:01 +0000

    Added Print Decision Endpoint

--- InternManagement.Api/Profiles/InternProfile.cs
+++ InternManagement.Api/Profiles/InternProfile.cs
@@ -4,15 +4,18 @@ using InternManagement.Api.Models;
 
 namespace InternManagement.Api.Profiles
 {
-  public class InternProfile : Profile
-  {
-    public InternProfile()
+    public class InternProfile : Profile
     {
-      CreateMap<InternDto, Intern>();
-      CreateMap<Intern, InternDto>();
-      CreateMap<Intern, InternListItemDto>()
-        .ForMember(dto => dto.FullName, opt => opt.MapFrom(intern => intern.FirstName + " " + intern.LastName))
-        .ForMember(dto => dto.Division, opt => opt.MapFrom(intern => intern.Division.Name));
+        public InternProfile()
+        {
+            CreateMap<InternDto, Intern>();
+            CreateMap<Intern, InternDto>();
+            CreateMap<Intern, InternListItemDto>()
+              .ForMember(dto => dto.FullName, opt => opt.MapFrom(intern => intern.FirstName + " " + intern.LastName))
+              .ForMember(dto => dto.Division, opt => opt.MapFrom(intern => intern.Division.Name));
+            CreateMap<Intern, DecisionDto>()
+              .ForMember(dto => dto.FullName, opt => opt.MapFrom(intern => intern.FirstName + " " + intern.LastName))
+              .ForMember(dto => dto.Department, opt => opt.MapFrom(intern => intern.Division.Department.Name));
+        }
     }
-  }
 }
--- InternManagement.Api/Services/IInternService.cs
+++ InternManagement.Api/Services/IInternService.cs
@@ -4,11 +4,12 @@ using InternManagement.Api.Dtos;
 
 namespace InternManagement.Api.Services
 {
-  public interface IInternService
-  {
-    Task<InternDto> AddInternAsync(InternDto dto);
-    Task<InternDto> GetInternByIdAsync(int id);
+    public interface IInternService
+    {
+        Task<InternDto> AddInternAsync(InternDto dto);
+        Task<InternDto> GetInternByIdAsync(int id);
 
-    Task<IEnumerable<InternListItemDto>> GetInternsAsync();
-  }
+        Task<IEnumerable<InternListItemDto>> GetInternsAsync();
+        Task<DecisionDto> PrintDecisionAsync(int id);
+    }
 }
