a2e94b9c3e0f45de9b7f1e3ac124ff5497e7ed6d
Ayoub Es-sehayli <ayoub1essehayli@outlook.com>
2021-08-13 10:18:07 +0000

    Fixed the search logic

--- InternManagement.Api/Repository/UserRepository.cs
+++ InternManagement.Api/Repository/UserRepository.cs
@@ -6,81 +6,84 @@ using Microsoft.EntityFrameworkCore;
 
 namespace InternManagement.Api.Repository
 {
-    public class UserRepository : IUserRepository
+  public class UserRepository : IUserRepository
+  {
+    private readonly InternContext _context;
+    public UserRepository(InternContext context)
     {
-        private readonly InternContext _context;
-        public UserRepository(InternContext context)
-        {
-            _context = context;
-        }
-        public async Task<User> AddUserAsync(User model)
-        {
-            await _context.Users.AddAsync(model);
-            await SaveChangesAsync();
-            return await _context.Users.OrderBy(user => user.Id).LastAsync();
-        }
-        public async Task<User> DeleteUserAsync(int id)
-        {
-            var user = await GetUserByIdAsync(id);
-            _context.Users.Remove(user);
-            await SaveChangesAsync();
-            return user;
-        }
-        public async Task<User> EditUserAsync(int id, User model)
-        {
-            var user = await GetUserByIdAsync(id);
-            if (user != null)
-            {
-                user.Email = model.Email;
-                user.FirstName = model.FirstName;
-                user.LastName = model.LastName;
-                user.Role = model.Role;
-                _context.Users.Update(user);
-                await SaveChangesAsync();
-            }
-            return user;
-        }
-        public async Task<User> FirstOrDefaultAsync(int id)
-        {
-            var user = await _context.Users.Where(i => i.Id == id).FirstOrDefaultAsync();
-            return user;
-        }
-
-        public async Task<int> GetUserCountAsync()
-        {
-            return await _context.Users.CountAsync();
-        }
-        public async Task<IEnumerable<User>> GetUsersAsync()
-        {
-            return await _context.Users.Select(user => new User
-            {
-                Id = user.Id,
-                LastName = user.LastName,
-                FirstName = user.FirstName,
-                Email = user.Email,
-            })
-            .ToListAsync();
-        }
-        public async Task SaveChangesAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
-        public async Task<User> UserExistsAsync(string username, string password)
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == username && x.Password == password);
-            return user;
-        }
-        public async Task<User> GetUserByIdAsync(int id)
-        {
-            var user = await _context.Users.Select(u => new User
-            {
-                Id = u.Id,
-                Email = u.Email,
-                FirstName = u.FirstName,
-                LastName = u.LastName,
-                Role = u.Role
-            }).Where(i => i.Id == id).FirstOrDefaultAsync();
-            return user;
-        }
+      _context = context;
     }
+    public async Task<User> AddUserAsync(User model)
+    {
+      await _context.Users.AddAsync(model);
+      await SaveChangesAsync();
+      return await _context.Users.OrderBy(user => user.Id).LastAsync();
+    }
+    public async Task<User> DeleteUserAsync(int id)
+    {
+      var user = await GetUserByIdAsync(id);
+      if (user != null)
+      {
+        _context.Users.Remove(user);
+        await SaveChangesAsync();
+      }
+      return user;
+    }
+    public async Task<User> EditUserAsync(int id, User model)
+    {
+      var user = await GetUserByIdAsync(id);
+      if (user != null)
+      {
+        user.Email = model.Email;
+        user.FirstName = model.FirstName;
+        user.LastName = model.LastName;
+        user.Role = model.Role;
+        _context.Users.Update(user);
+        await SaveChangesAsync();
+      }
+      return user;
+    }
+    public async Task<User> FirstOrDefaultAsync(int id)
+    {
+      var user = await _context.Users.Where(i => i.Id == id).FirstOrDefaultAsync();
+      return user;
+    }
+
+    public async Task<int> GetUserCountAsync()
+    {
+      return await _context.Users.CountAsync();
+    }
+    public async Task<IEnumerable<User>> GetUsersAsync()
+    {
+      return await _context.Users.Select(user => new User
+      {
+        Id = user.Id,
+        LastName = user.LastName,
+        FirstName = user.FirstName,
+        Email = user.Email,
+      })
+      .ToListAsync();
+    }
+    public async Task SaveChangesAsync()
+    {
+      await _context.SaveChangesAsync();
+    }
+    public async Task<User> UserExistsAsync(string username, string password)
+    {
+      var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == username && x.Password == password);
+      return user;
+    }
+    public async Task<User> GetUserByIdAsync(int id)
+    {
+      var user = await _context.Users.Select(u => new User
+      {
+        Id = u.Id,
+        Email = u.Email,
+        FirstName = u.FirstName,
+        LastName = u.LastName,
+        Role = u.Role
+      }).Where(i => i.Id == id).FirstOrDefaultAsync();
+      return user;
+    }
+  }
 }
